2430c338f187b8176e67983ae60ff18c112b8f70
Mike Dettmer <mike@mikedettmer.com>
2016-03-10 22:03:14 -0500

    added route for day15

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express'),
     path = require('path'),
     http = require('http'),
     app = express(),
-    day8 = require('./routes/day8');
+    day8 = require('./routes/day8'),
+    day15 = require('./routes/day15');
 
 app.set('port', 7080);
 
 app.use('/day8/', day8);
+app.use('/day15/', day15);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
