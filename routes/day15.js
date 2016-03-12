0b2834543380822f370fe0511ad7220676344a37
Mike Dettmer <mike@mikedettmer.com>
2016-03-12 17:57:29 -0500

    fixed typo in day15 route

--- routes/day15.js
+++ routes/day15.js
@@ -7,15 +7,13 @@ router.get('/api/notes', function(req, res) {
   Day15.find({}, function(err, notes) {
     if(err) { res.status(500).send('Retrieving notes broke'); }
     else {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.json(notes);
     }
   });
 });
 
 router.post('/api/notes', function(req, res) {
-  var newNote = new Note({
+  var newNote = new Day15({
     author: req.body.author,
     comment: req.body.comment,
     date: Date.now()
@@ -24,8 +22,6 @@ router.post('/api/notes', function(req, res) {
   newNote.save(function(err) {
     if(err) { res.status(500).send('Saving note failed!'); }
     else {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.status(200).send('Worked!');
     }
   });
